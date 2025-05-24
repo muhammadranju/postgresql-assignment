af7bdb1e5eacc68721078f6dc588c1f698a164f9
Muhammad Ranju <mdranju23@gmail.com>
2025-05-24 23:48:53 +0600

    Add SQL script to create conservation_db and define initial tables

--- index.sql
+++ index.sql
@@ -1,3 +1,26 @@
 -- Active: 1747464306999@@127.0.0.1@5432@conservation_db
 
 CREATE DATABASE conservation_db;
+
+CREATE TABLE rangers (
+    ranger_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    region VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(100) NOT NULL,
+    scientific_name VARCHAR(150) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE sightings (
+    sighting_id SERIAL PRIMARY KEY,
+    species_id INT REFERENCES species (species_id),
+    ranger_id INT REFERENCES rangers (ranger_id),
+    location VARCHAR(100) NOT NULL,
+    sighting_time TIMESTAMP NOT NULL DEFAULT NOW(),
+    notes TEXT
+);
