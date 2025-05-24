2aa4187006f2de46d2d6ef26dd852c91ccd50c47
Muhammad Ranju <mdranju23@gmail.com>
2025-05-24 23:50:56 +0600

    Add query to retrieve the most recent sightings with species and ranger details

--- index.sql
+++ index.sql
@@ -131,3 +131,11 @@ WHERE
             species_id
         FROM sightings
     );
+
+SELECT sp.common_name, s.sighting_time, r.name
+FROM
+    sightings s
+    JOIN species sp ON s.species_id = sp.species_id
+    JOIN rangers r ON s.ranger_id = r.ranger_id
+ORDER BY s.sighting_time DESC
+LIMIT 2;
